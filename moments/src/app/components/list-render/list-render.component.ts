f18759e5c6b74584e2f80dad227370b43c7760b7
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-05 23:41:23 -0300

    The importance of interfaces in Angular

--- moments/src/app/components/list-render/list-render.component.ts
+++ moments/src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name: "Frida", type: "Dog"},
-    {name: "Bob", type: "Horse"}
+  animals: Animal[] = [
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: "Cat", age: 5},
+    {name: "Frida", type: "Dog", age: 3},
+    {name: "Bob", type: "Horse", age: 5}
   ]
+
+  animalDetails = ''
+
+  showAge(animal: Animal): void{
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
 }
