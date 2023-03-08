d21cfaedd8e7fd31e3dbde9981851fb6a3aec4b7
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-08 02:23:33 -0300

    Angular services

--- moments/src/app/components/list-render/list-render.component.ts
+++ moments/src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -17,7 +19,14 @@ export class ListRenderComponent {
 
   animalDetails = ''
 
+  constructor(private listService: ListService){}
+
   showAge(animal: Animal): void{
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
+
+  removeAnimal(animal: Animal){
+    console.log('Removendo animal...')
+    this.animals = this.listService.remove(this.animals, animal)
   }
 }
